0f27afb9b2a59bb0443ad5d8360b0b3ab4714ed6
Therese <teddytiyi@gmail.com>
2023-02-11 18:37:54 +0200

    adding Formvalidation.js

---
+++ JS/Form-validation.js
@@ -0,0 +1,20 @@
+const formelement = document.getElementsByTagName('form');// eslint-disable-line
+const name = document.getElementById('username');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+formelement[0].addEventListener('input', () => {
+  const data = {
+    username: name.value,
+    useremail: email.value,
+    usermessage: message.value,
+  };
+  localStorage.setItem('userInput', JSON.stringify(data));
+});
+
+window.addEventListener('load', () => {
+  const data = JSON.parse(localStorage.getItem('userInput'));
+  name.value = data.username;
+  email.value = data.useremail;
+  message.value = data.usermessage;
+});
